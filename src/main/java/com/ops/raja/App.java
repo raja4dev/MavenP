58d11250696b350b8a8e5d572cb0fd867c468505
mrajadev <mrajadev123@gmail.com>
2018-07-21 07:26:15 +0530

    Added Maven Project

---
+++ src/main/java/com/ops/raja/App.java
@@ -0,0 +1,13 @@
+package com.ops.raja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
